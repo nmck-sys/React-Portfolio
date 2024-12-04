7abd70456c50ce82cad444aaaa8c5e9e3a5a8e61
HA_GO\antic <nmckinzie@smcm.edu>
2024-12-03 20:59:00 -0600

    created form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,52 @@
-const Contact = () => {
-    return (
-        <section>
-        <h2>Contact</h2>
-        <p>
-            I am a fullstack developer who has been learning Javascript since early 2024. 
-        </p>
-        </section>
-    )
+import { useState } from 'react';
+
+export default function Contact() {
+    const [name, setFormName] = useState('');
+    const [email, setFormEmail] = useState('');
+    const [message, setFormMessage] = useState('');
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [submissionStatus, setSubmissionStatus] = useState('');
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+
+        if (name === 'name') setFormName(value);
+        if (name === 'email') setFormEmail(value);
+        if (name === 'message') setFormMessage(value);
+
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setIsSubmitting(true);
+
+    if (!name || !email || !message) {
+        setSubmissionStatus('Please provide info.');
+        setIsSubmitting(false);
+        return;
+    }
 }
+    
 
-
-
-
-
-
-
-
-export default Contact;
+return (
+    <div className="contact-form">
+        <h1>Contact Me!</h1>
+        <div className="contact-container" onSubmit={handleFormSubmit}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" onChange={handleInputChange} />
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" onChange={handleInputChange}/>
+            <label htmlFor="message">Message</label>
+            <textarea id="message" onChange={handleInputChange}></textarea>
+            <button className="submit-button">Submit</button>
+        </div>
+        <div className="form-group">
+          <button className="submit-button" type="submit" disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </button>
+        </div>
+        {submissionStatus && <p>{submissionStatus}</p>}
+    </div>
+);
+}
